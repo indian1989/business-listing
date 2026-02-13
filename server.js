e2e94fb389dadc929b2399626eb6251c8dda995e
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-13 06:26:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,32 +12,3 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
 console.log("Server running on port " + PORT);
 });
-
-// Add business (POST)
-app.post("/add-business", (req, res) => {
-const data = JSON.parse(fs.readFileSync(DATA_FILE));
-
-const newBusiness = {
-id: Date.now(),
-name: req.body.name,
-category: req.body.category,
-city: req.body.city,
-phone: req.body.phone,
-status: "pending"
-};
-
-data.push(newBusiness);
-
-fs.writeFileSync(DATA_FILE, JSON.stringfy(data, null, 2));
-
-res.send("<h2>Business submitted for approval</h2><a href='/'>Go Home</a>);
-});
-
-// API - get businesses by category
-app.get("/api/businesses", (req, res) => {
-const category = req.query.category;
-const data = JSON.parse(fs..readFileSync(DATA_FILE));
-
-const filtered = data.filter(b => b.category === category);
-res.json(filtered);
-});
