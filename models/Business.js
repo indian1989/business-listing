eac1cab482db8a1104b036d6153d3a8ec9603f68
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-14 23:33:51 +0530

    Create Business model with listing schema

--- models/Business.js
+++ models/Business.js
@@ -1,1 +1,28 @@
-Business.js
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  businessName: String,
+  ownerName: String,
+  category: String,
+  city: String,
+  phone: String,
+  description: String,
+
+  addedBy: {
+    type: String,
+    enum: ["admin", "provider"]
+  },
+
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending"
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Listing", listingSchema);
